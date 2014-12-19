cb3ca00d77ce04759553347c1a74160379461c3c
rj9935 <rj9935@telus.net>
2014-12-19 12:53:59 -0800

    Simplified matrix verification functionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,21 @@
-## These 2 functions demonstrate the performance improvement possible by using
-## caching. From the programming assignment specification, it is not clear if
-## additional protection against changes in the cached matrix beyond those in
-## the example code are required. Therefore a rudimentary checksum is used here 
-## to provide some protection against changes to the cached matrix - NB to 
-## protect against malicious changes a more sophisticated checksum (then the 
-## grand sum implemented here) will be required (and will have to be applied
-## to the cached inverse matrix as well). A simple test procedure testing all
-## of this functionality is given below at the foot of this file.
+## These functions demonstrate the performance improvement possible by using
+## caching. In the programming assignment it's not clear if more protection 
+## against cached matrix changes than those in the example code are needed. So a 
+## rudimentary checksum is used here. To protect against malicious changes a 
+## better checksum could be used (and applied to the inverse matrix too). 
+## A simple test procedure for all this is at the foot of this file.
 
-## The makeCacheMatrix function takes a matrix as input and creates an object 
+## The computeCs function computes and returns a checksum - the one
+## implemented here is very simple (grand sum) but there are R packages with 
+## more sophisticated implementations (eg MD5) if needed
+computeCs <- function(x){
+    ## Compute and cache the checksum from the matrix x - NB the assignment
+    ## operator is used here (not superassignment) since it is vital not to 
+    ## overwrite the cached value of cs in the cachedMatrix object.
+    cs <- sum(rowSums(x)) 
+}
+
+## The makeCacheMatrix function inputs a matrix and creates an object 
 ## containing the matrix, the matrix checksum, the inverse matrix (initially 
 ## set to NULL)  and a list of functions that can be applied to these items:
 ##
@@ -27,8 +34,8 @@ makeCacheMatrix <- function(x = matrix()) {
     ## On instantiation the inverse matrix has not yet been computed
     invX <- NULL
     
-    ## Compute and cache the matrix checksum (simple grand sum in this case)
-    cs <- sum(rowSums(x))
+    ## Compute and cache the matrix checksum
+    cs <- computeCs(x)
     
     set <- function(y){
         ## Cache the new matrix
@@ -36,32 +43,31 @@ makeCacheMatrix <- function(x = matrix()) {
         ## If the matrix is changed then the inverse is incorrect
         invX <<- NULL
         ## The checksum also requires to be recomputed and cached
-        cs <<- sum(rowSums(x))
+        cs <<- computeCs(x)
         }
     
     get <- function() x  
     getcs <- function() cs    
     setinverse <- function(inverse) invX <<- inverse
-    getinverse <- function() invX
-    
+    getinverse <- function() invX    
+        
     change <- function(y){
-        x <<- y   
         ## Since this function simulates unexpected changes in the values of the
-        ## cached matrix elements the invX & cs items are left unchanged
+        ## cached matrix elements the invX & cs items are left unchanged ie the
+        ## cached inverse is no longer the inverse of the cached matrix
+         x <<- y   
         }
     
     list(set = set, get = get, getcs = getcs,
          setinverse = setinverse, getinverse = getinverse, change = change)
 }
 
-## The cacheSolve function takes an object created by makeCacheMatrix and
-## returns the inverse of the matrix contained in the object. The function
-## checks to see if the cached matrix has been changed since the checksum was
-## computed and if it has prompts the user to re-enter the matrix and then 
-## returns NULL for the the inverse. if not, then the function checks if inverse
-## is already held in cache and returns the stored value if it is. If not, it 
-## calls solve() to invert the matrix, puts the newly generated inverse in the 
-## cache and returns it. 
+## The cacheSolve function takes a makeCacheMatrix object and returns the 
+## inverse of the matrix cached in it. It checks to see if the cached matrix has
+## changed since the checksum was computed and if so, prompts the user to 
+## re-enter the matrix and returns NULL for the the inverse. if not, then it
+## checks if the inverse is already in cache and, if so, returns the cached value
+## If not, it calls solve() to invert the matrix, caches and returns the inverse
 cacheSolve <- function(x, ...) {
 
     ## Get the cached values of the matrix, the inverse & the checksum
@@ -69,8 +75,8 @@ cacheSolve <- function(x, ...) {
     myinvX <- x$getinverse()
     mycs <- x$getcs()
         
-    ## Test if the computed checksum for the matrix agrees with the cached value
-    if(abs(mycs-sum(rowSums(myData)))<1.0E-10){
+    ## Test if the checksum for the current matrix agrees with the cached value
+    if(abs(mycs-computeCs(myData))<1.0E-10){
 
         ## Test to see if the inverse is in cache
         if(!is.null(myinvX)){
@@ -105,7 +111,7 @@ cacheSolve <- function(x, ...) {
 ## > cm$set(matrix(rnorm(4000000),2000,2000))
 ## > icm <- cacheSolve(cm)
 ## > icm <- cacheSolve(cm)
-## > cm$change (matrix(rnorm(4000000),2000,2000))
+## > cm$change(matrix(rnorm(4000000),2000,2000))
 ## > icm <- cacheSolve(cm)
 ## > icm <- cacheSolve(cm)
 ## > cm$set(matrix(rnorm(4000000),2000,2000))
